1d5063be3b476ed090e5d976502036523d28c808
Neokoldstudio <nightmare.vid@gmail.com>
2023-11-30 18:13:12 -0500

    first intuition test for Currency, still have to check borders

--- src/test/CurrencyTest.java
+++ src/test/CurrencyTest.java
@@ -1,10 +1,17 @@
 package test;
 
+import currencyConverter.Currency;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class CurrencyTest {
     @Test
-    public void testConvert() {
+    public void testConvertCorrect() {
+        assertEquals(67.0, Currency.convert(100.0, 0.67), 0.00001);
+    }
 
+    @Test
+    public void testConvertFalse() {
+        assertNotEquals(50, Currency.convert(100.0, 0.25), 0.00001);
     }
 }
